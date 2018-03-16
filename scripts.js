ca4fda2db4fa3b665f050b2a378af5e9e21dd959
vladmv96 <33199411+vladmv96@users.noreply.github.com>
2018-03-16 16:32:26 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,14 +1,10 @@
 
-
 jQuery(document).ready(function() {
-$("nav").on("click","a", function (event) {
-        event.preventDefault();
-
-        var id  = $(this).attr('href'),
-
-         top = $(id).offset().top - 59;
-
-         $('body,html').animate({scrollTop: top}, 800);
-
-    });
+var $page = $('html, body');
+$('a[href*="#"]').click(function() {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top - 30
+    }, 800);
+    return false;
+});
 });
